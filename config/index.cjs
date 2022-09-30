ad1329109fc1c0c2a955fc99b6a150dbd299e328
dai99999 <114736487+dai99999@users.noreply.github.com>
2022-09-30 23:38:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,7 +24,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oU3Ej6fsrqcC6_9K7jFCyQmO58S4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 't7Il_nNNwqiBCJ0FvRSXjP-xWa1FKzYWtXQrgw64sVw',
+      useTemplateId: 'AQCWkK_YImBal9bZ4_TmKUOXJWQyu8pLste5EiPHN_M',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-08',
       festivals: [
